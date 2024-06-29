f702ae8b02a9ecd84ab6f2a78fc62033fbeddc51
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-06-29 11:34:40 +0300

    SEADO course - 2 exam preparations and 1 past exam

---
+++ Softuni - Software Engineering and DevOps/Past Exam/tests/ui/shoppingList.test.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('Check cookbook page', async ({ page }) => {
+    await page.goto('http://localhost:8090/cookbook');
+    const list = await page.$('ul');
+    expect(list).toBeTruthy();
+  });
+  
